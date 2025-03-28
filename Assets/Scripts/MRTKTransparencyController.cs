964ae3abff3873a98ec0be554b546f79b6ac30cb
IRTrackingOrgans <n.t.degroot-15@prinsesmaximacentrum.nl>
2025-03-28 13:08:18 +0100

    Added new files

---
+++ Assets/Scripts/MRTKTransparencyController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class MRTKTransparencyController : MonoBehaviour
+{
+    public PinchSlider transparencySlider; // Reference to the MRTK Slider
+    public GameObject targetObject;        // Reference to the GameObject to control
+
+    private Material targetMaterial;
+
+    void Start()
+    {
+        // Get the material of the target object
+        if (targetObject != null)
+        {
+            Renderer renderer = targetObject.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                targetMaterial = renderer.material;
+            }
+        }
+
+        // Add listener to the slider
+        if (transparencySlider != null)
+        {
+            transparencySlider.OnValueUpdated.AddListener(SetTransparency);
+        }
+    }
+
+    void SetTransparency(SliderEventData data)
+    {
+        if (targetMaterial != null)
+        {
+            Color color = targetMaterial.color;
+            color.a = data.NewValue; // Set the alpha value
+            targetMaterial.color = color;
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Remove listener to avoid memory leaks
+        if (transparencySlider != null)
+        {
+            transparencySlider.OnValueUpdated.RemoveListener(SetTransparency);
+        }
+    }
+}
